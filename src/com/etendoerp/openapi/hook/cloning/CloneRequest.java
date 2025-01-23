26031cc00fd3c0ba2319d271cdfcd66c2f65b129
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2025-01-23 11:17:35 -0300

    Feature ETP-1023: Add Clonning hooks for Flows and Requests

---
+++ src/com/etendoerp/openapi/hook/cloning/CloneRequest.java
@@ -0,0 +1,73 @@
+package com.etendoerp.openapi.hook.cloning;
+
+import static com.etendoerp.openapi.hook.cloning.CloneFlow.removeModuleInClonedChildren;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.openbravo.base.structure.BaseOBObject;
+import org.openbravo.client.kernel.ComponentProvider;
+import org.openbravo.dal.service.OBDal;
+
+import com.etendoerp.openapi.data.OpenAPIRequest;
+import com.smf.jobs.hooks.CloneRecordHook;
+
+@ApplicationScoped
+@ComponentProvider.Qualifier(OpenAPIRequest.ENTITY_NAME)
+public class CloneRequest extends CloneRecordHook {
+
+  /**
+   * Determines whether to copy child records.
+   * <p>
+   * This method always returns true, indicating that child records should be copied.
+   *
+   * @param uiCopyChildren
+   *     A boolean indicating if the UI requests to copy children.
+   * @return true, indicating that child records should be copied.
+   */
+  @Override
+  public boolean shouldCopyChildren(boolean uiCopyChildren) {
+    return true;
+  }
+
+  /**
+   * Pre-copy hook.
+   * <p>
+   * This method is called before the original record is copied. It returns the original record without modifications.
+   *
+   * @param originalRecord
+   *     The original record to be copied.
+   * @return The original record.
+   */
+  @Override
+  public BaseOBObject preCopy(BaseOBObject originalRecord) {
+    return originalRecord;
+  }
+
+  /**
+   * Post-copy hook.
+   * <p>
+   * This method is called after the original record is copied. It modifies the cloned record by setting a new name and nullifying the module.
+   * The cloned record is then saved, flushed, and refreshed in the database.
+   *
+   * @param originalRecord
+   *     The original record that was copied.
+   * @param newRecord
+   *     The newly created cloned record.
+   * @return The modified cloned record.
+   */
+  @Override
+  public BaseOBObject postCopy(BaseOBObject originalRecord, BaseOBObject newRecord) {
+    OpenAPIRequest originalReq = (OpenAPIRequest) originalRecord;
+    OpenAPIRequest cloneReq = (OpenAPIRequest) newRecord;
+    cloneReq.setName("Copy of " + originalReq.getName());
+    cloneReq.setModule(null);
+
+    OBDal.getInstance().save(cloneReq);
+    OBDal.getInstance().flush();
+    removeModuleInClonedChildren(cloneReq);
+
+    OBDal.getInstance().refresh(cloneReq);
+
+    return cloneReq;
+  }
+}
